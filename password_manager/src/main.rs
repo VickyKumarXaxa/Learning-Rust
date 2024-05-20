20012bbdd6f8f49eb746c7fe33a7b323cb5736a2
Vicky Kumar Xaxa <vickykumar835302@gmail.com>
2024-05-20 17:26:38 +0530

    learning basic crud operations

--- password_manager/src/main.rs
+++ password_manager/src/main.rs
@@ -1,4 +1,12 @@
+use rusqlite::{Connection, Result};
 use std::io::stdin;
+
+#[derive(Debug)]
+struct User {
+    id: i32,
+    name: String,
+    master_password: String,
+}
 
 fn display_options_list() {
     const SCREEN_TEXT: [&str; 4] = [
@@ -20,7 +28,39 @@ fn add_password() {}
 
 fn password_manager_operations(selected_operation: u8, logged: bool) {}
 
-fn main() {
+fn main() -> Result<()> {
+    let path = "./my_db.db3";
+    let conn = Connection::open(path)?;
+
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS users ( id INTEGER PRIMARY KEY, user_name TEXT NOT NULL UNIQUE, master_password TEXT NOT NULL)",
+        (),
+    )?;
+
+    let user1 = User {
+        id: 0,
+        name: "user1".to_string(),
+        master_password: "password".to_string(),
+    };
+
+    conn.execute(
+        "INSERT INTO users (user_name, master_password) VALUES (?1, ?2)",
+        (&user1.name, &user1.master_password),
+    )?;
+
+    let mut stmt = conn.prepare("SELECT id, user_name, master_password FROM users")?;
+    let user_iter = stmt.query_map([], |row| {
+        Ok(User {
+            id: row.get(0)?,
+            name: row.get(1)?,
+            master_password: row.get(2)?,
+        })
+    })?;
+
+    for user in user_iter {
+        println!("Found users {:?}", user.unwrap());
+    }
+
     let mut user_input = String::new();
     loop {
         display_options_list();
@@ -30,4 +70,6 @@ fn main() {
         println!("{}", user_input.trim());
         user_input.clear();
     }
+
+    Ok(())
 }
